e827dd32e5c53374c7d930d588713be2d283eb49
Tim Van Wassenhove <github@timvw.be>
2016-03-21 21:43:39 +0100

    further work to make this backwards compatible with existing generator

--- app/index.js
+++ app/index.js
@@ -8,10 +8,7 @@ var uuid = require('uuid');
 var spawn = require('child_process').spawn;
 var spawnSync = require('child_process').spawnSync;
 var request = require('request');
-//var xmldom = require("xmldom");
-//var wrench = require('wrench');
 
-var _0777 = parseInt('0777', 8);
 var greeting =
   "                          \r\n" +
   "                _         \r\n" +
@@ -21,84 +18,14 @@ var greeting =
   " |___/\___\__,_|_|\__,_|  \r\n" +
   "                          \r\n" ;
 
-  fs.mkdirParent = function(dirPath, mode, callback) {
-    //Call the standard fs.mkdir
-    fs.mkdir(dirPath, mode, function(error) {
-      //When it fail in this way, do the custom steps
-      if (error && error.errno === 34) {
-        //Create all the parents recursively
-        fs.mkdirParent(path.dirname(dirPath), mode, callback);
-        //And then the directory
-        fs.mkdirParent(dirPath, mode, callback);
-      }
-      //Manually run the callback since we used our own callback to do all these
-      callback && callback(error);
-    });
-  };
-
-  fs.mkdirParentSync = function sync (p, opts, made) {
-      if (!opts || typeof opts !== 'object') {
-          opts = { mode: opts };
-      }
-
-      var mode = opts.mode;
-      var xfs = opts.fs || fs;
-
-      if (mode === undefined) {
-          mode = _0777 & (~process.umask());
-      }
-      if (!made) made = null;
-
-      p = path.resolve(p);
-
-      try {
-          xfs.mkdirSync(p, mode);
-          made = made || p;
-      }
-      catch (err0) {
-          switch (err0.code) {
-              case 'ENOENT' :
-                  made = sync(path.dirname(p), opts, made);
-                  sync(p, opts, made);
-                  break;
-
-              // In the case of any other error, just see if there's a dir
-              // there already.  If so, then hooray!  If not, then something
-              // is borked.
-              default:
-                  var stat;
-                  try {
-                      stat = xfs.statSync(p);
-                  }
-                  catch (err1) {
-                      throw err0;
-                  }
-                  if (!stat.isDirectory()) throw err0;
-                  break;
-          }
-      }
-
-      return made;
-  };
-
-
 var ScalaGenerator = yeoman.generators.NamedBase.extend({
 
     username: 'timvw',
     repo: 'generator-scala',
     branch: 'templates',
 
-    ACTION_CREATE_STANDALONE_PROJECT: 1,
-    ACTION_ADD_PROJECT_TO_SOLUTION: 2,
-    ACTION_CREATE_EMPTY_SOLUTION: 3,
-    ACTION_ADD_REFERENCE_TO_PROJECT: 4,
-
     constructor: function() {
         yeoman.generators.Base.apply(this, arguments);
-    },
-
-    _isOnWindows : function() {
-        return /^win/.test(process.platform);
     },
 
     _download : function(t, done, reload) {
@@ -109,17 +36,6 @@ var ScalaGenerator = yeoman.generators.NamedBase.extend({
 
     _getTemplateDirectory : function() {
         return path.join(this.cacheRoot(), this.username, this.repo, this.branch);
-    },
-
-    _execManaged : function(file, args, options) {
-        if(this._isOnWindows()){
-            return spawn(file, args, options);
-        }
-        else {
-            var monoArgs = [file];
-            monoArgs = monoArgs.concat(args);
-            return spawn('mono', monoArgs, options);
-        }
     },
 
     _saveSHA : function (p, sha, old) {
@@ -167,9 +83,6 @@ var ScalaGenerator = yeoman.generators.NamedBase.extend({
         });
     },
 
-
-
-
     init: function() {
         this.log(greeting);
         this.log('Welcome to the perfect ' + chalk.red('Scala') + ' generator!');
@@ -180,6 +93,7 @@ var ScalaGenerator = yeoman.generators.NamedBase.extend({
         this._getSHA(old, p, done);
     },
 
+    /*
     askFor: function() {
         var done = this.async();
         var prompts = [{
@@ -197,104 +111,73 @@ var ScalaGenerator = yeoman.generators.NamedBase.extend({
             done();
         }.bind(this));
     },
+    */
 
-    askForProject: function() {
-        if (this.action === this.ACTION_ADD_REFERENCE_TO_PROJECT)
-            return;
-
+    askForTemplate: function() {
         var done = this.async();
-        if(this.action !== this.ACTION_CREATE_EMPTY_SOLUTION) {
-            var p = path.join(this._getTemplateDirectory(), 'templates.json')
-            var choices = JSON.parse(fs.readFileSync(p, "utf8"));
-            var prompts = [{
-                type: 'list',
-                name: 'type',
-                message: 'What type of application do you want to create?',
-                choices: choices.Templates
-            }];
 
-            this.prompt(prompts, function(props) {
-                this.type = props.type;
-                done();
-            }.bind(this));
-        }
-        else {
+        var p = path.join(this._getTemplateDirectory(), 'templates.json');
+        var choices = JSON.parse(fs.readFileSync(p, "utf8"));
+        var prompts = [{
+            type: 'list',
+            name: 'type',
+            message: 'What type of application do you want to create?',
+            choices: choices.Templates
+        }];
+
+        this.prompt(prompts, function(props) {
+            this.type = props.type;
             done();
-        }
+        }.bind(this));
     },
 
     askForName: function() {
-        if (this.action === this.ACTION_ADD_REFERENCE_TO_PROJECT)
-            return;
+      if(this.args.length >= 1) {
+        this.applicationName = this.args[0];
+        return;
+      }
 
-        var done = this.async();
-        var prompts = [{
-            name: 'applicationName',
-            message: 'What\'s the name of your application?',
-            default: this.type
+      var done = this.async();
+      var prompts = [{
+          name: 'applicationName',
+          message: 'What\'s the name of your application?',
+          default: this.type
+      }];
+
+      this.prompt(prompts, function(props) {
+          this.applicationName = props.applicationName;
+          done();
+      }.bind(this));
+    },
+
+    askForScalaVersion: function() {
+      if(this.args.length >= 2) {
+        this.scalaVersion = this.args[1];
+        return;
+      }
+
+      var done = this.async();
+      var prompts = [{
+            type: 'list',
+            name: 'scalaversion',
+            message: 'Which version of Scala do you want to use?',
+            choices: [
+              { name: '2.10.4', value: '2.10.4' },
+              { name: '2.10.5', value: '2.10.5' },
+              { name: '2.11.7', value: '2.11.7' },
+              { name: '2.11.8', value: '2.11.8' }],
+            default: '2.11.8'
         }];
+
         this.prompt(prompts, function(props) {
-            this.templatedata.namespace = props.applicationName;
-            this.templatedata.applicationname = props.applicationName;
-            this.templatedata.guid = uuid.v4();
-            if(this.action === this.ACTION_ADD_PROJECT_TO_SOLUTION) {
-                this.templatedata.packagesPath = "../packages"
-                this.templatedata.paketPath = "../.paket"
-            }
-            else {
-                this.templatedata.packagesPath = "packages"
-                this.templatedata.paketPath = ".paket"
-            }
-            this.applicationName = props.applicationName;
+            this.scalaVersion = props.scalaVersion;
             done();
         }.bind(this));
     },
 
-/*
-    askForPaket: function() {
-        if (this.action === this.ACTION_ADD_REFERENCE_TO_PROJECT)
-            return;
 
-        var done = this.async();
-        var prompts = [{
-            type: 'list',
-            name: 'paket',
-            message: 'Do You want to use Paket?',
-            choices: [{"name": "Yes", "value": true}, {"name": "No", "value": false}]
-        }];
-        this.prompt(prompts, function(props) {
-            this.paket = props.paket;
-            done();
-        }.bind(this));
-
-    },
-    */
 
 /*
-    askForFake: function() {
-        if (this.action === this.ACTION_ADD_REFERENCE_TO_PROJECT)
-            return;
-
-        var done = this.async();
-        var prompts = [{
-            type: 'list',
-            name: 'fake',
-            message: 'Do You want to use FAKE?',
-            choices: [{"name": "Yes", "value": true}, {"name": "No", "value": false}]
-        }];
-
-        if (this.paket) {
-            this.prompt(prompts, function(props) {
-                this.fake = props.fake;
-                done();
-            }.bind(this));
-            return;
-        }
-
-        done();
-    },
-    */
-
     _copy: function(dirPath, targetDirPath){
 
         var files = fs.readdirSync(dirPath);
@@ -313,177 +196,17 @@ var ScalaGenerator = yeoman.generators.NamedBase.extend({
             }
         }
     },
+    */
 
+      /*
     writing: function() {
-        if (this.action === this.ACTION_ADD_REFERENCE_TO_PROJECT)
-            return;
-
         var log = this.log;
-        var p;
-        if (this.action === this.ACTION_CREATE_EMPTY_SOLUTION){
-            p = path.join(this._getTemplateDirectory(), 'sln')
-        }
-        else {
-            p = path.join(this._getTemplateDirectory(), this.type);
-        }
+        var p = path.join(this._getTemplateDirectory(), this.type);
         this._copy(p, this.applicationName);
-        if(this.paket) {
-            var bpath;
-            if(this.action !== this.ACTION_ADD_PROJECT_TO_SOLUTION) {
-                bpath = path.join(this.applicationName, ".paket", "paket.bootstrapper.exe" );
-            }
-            else {
-                bpath = path.join(".paket", "paket.bootstrapper.exe" );
-            }
-            var p = path.join(this._getTemplateDirectory(), ".paket", "paket.bootstrapper.exe");
-            this.copy(p, bpath);
-        }
-        if(this.fake) {
-            if (this.action !== this.ACTION_ADD_PROJECT_TO_SOLUTION){
-                var fakeSource = path.join(this._getTemplateDirectory(), ".fake");
-                this._copy(fakeSource, this.applicationName);
-            }
-        }
     },
+            */
 
-    _getProjectFile: function() {
-        var dirPath = this.destinationRoot();
-        var files = fs.readdirSync(dirPath);
-
-        var projectFile;
-
-        for(var i in files)
-        {
-            var f = files[i];
-            var fp = path.join(dirPath, f);
-
-            if (fp.endsWith(".fsproj"))
-            {
-                projectFile = fp;
-            }
-        }
-
-        return projectFile;
-    },
-
-    _getProjectFiles: function() {
-        var result = [];
-        var dirPath = path.join(this.destinationRoot(), "..");
-        var files = wrench.readdirSyncRecursive(dirPath);
-
-        for(var i in files)
-        {
-            var f = files[i];
-            if (f.endsWith(".fsproj"))
-            {
-                result.push(f);
-            }
-        }
-
-        return result;
-    },
-
-
-    _askForReference: function(projectFiles, onChoose) {
-        var choices = projectFiles.map(function(s) {
-            return {"name": s, "value": "./../" + s};
-        });
-
-        var prompts = [{
-            type: 'list',
-            name: 'projectToReference',
-            message: 'Which project should be referenced?',
-            choices: choices
-        }];
-        this.prompt(prompts, function(props) {
-            onChoose(props.projectToReference);
-        }.bind(this));
-    },
-
-    _addReference: function(done) {
-        var log = this.log;
-        var projectFile = this._getProjectFile();
-        var fs = this.fs;
-
-        if (projectFile === undefined)
-        {
-            this.log("No project file in local folder found");
-            done();
-            return;
-        }
-
-        this.log("Project file: " + projectFile);
-
-        var projectFiles = this._getProjectFiles();
-
-        this._askForReference(projectFiles, function(selectedFile) {
-
-            var projectFileContent = fs.read(projectFile);
-            var domParser = new xmldom.DOMParser();
-
-            var projectXml = domParser.parseFromString(projectFileContent, 'text/xml');
-
-            var projectReferenceItemGroup;
-
-            var itemGroupNodes = projectXml.getElementsByTagName("ItemGroup");
-
-            for (var c in itemGroupNodes)
-            {
-                var node = itemGroupNodes[c];
-
-                for (var cc in node.childNodes)
-                {
-                    var itemGroupNode = node.childNodes[cc];
-                    if (itemGroupNode.nodeName == "ProjectReference")
-                    {
-                        projectReferenceItemGroup = node;
-                        break;
-                    }
-                }
-            }
-
-            if (projectReferenceItemGroup === undefined)
-            {
-                var newItemGroup = projectXml.createElement("ItemGroup");
-                var lastItemGroup = itemGroupNodes[itemGroupNodes.length-1];
-                projectReferenceItemGroup = projectXml.insertBefore(newItemGroup, lastItemGroup);
-            }
-
-            var alreadyReferenced = false;
-            for (var cc in projectReferenceItemGroup.childNodes)
-            {
-                var itemGroupNode = projectReferenceItemGroup.childNodes[cc];
-                if (itemGroupNode.nodeName == "ProjectReference")
-                {
-                    if (itemGroupNode.getAttribute("Include") === selectedFile)
-                    {
-                        alreadyReferenced = true;
-                    }
-                }
-            }
-
-            if (alreadyReferenced) {
-                log(selectedFile + " is already referenced");
-            }
-            else {
-                var projectReferenceNode = projectXml.createElement("ProjectReference");
-                projectReferenceNode.setAttribute("Include", selectedFile);
-
-                projectReferenceItemGroup.appendChild(projectReferenceNode);
-
-                var xmlSerialzier = new xmldom.XMLSerializer()
-                var xml = xmlSerialzier.serializeToString(projectXml);
-                log("Please press Y for updating the existing file");
-
-                //log(xml);
-                fs.write(projectFile, xml);
-            }
-
-            done();
-        });
-    },
-
-
+/*
     install: function() {
         var log = this.log
         var done = this.async();
@@ -495,34 +218,23 @@ var ScalaGenerator = yeoman.generators.NamedBase.extend({
 
         var generator = this;
 
-        var isWin = this._isOnWindows();
-
-        if (action === this.ACTION_ADD_REFERENCE_TO_PROJECT)
-        {
-            this._addReference(done);
-            return;
-        }
-
-        if(this.fake) {
-            if (!this._isOnWindows()) {
-                var buildShPath = path.join(dest, appName, 'build.sh');
-                var chmodProc = spawnSync('chmod', ['+x', buildShPath], {cwd: dest});
-            }
-        }
-
-
         done();
     },
+    */
 
+
+    summary: function() {
+      this.log('ready to create an application ' +
+        ' type: ' + this.type +
+        ' named: ' + this.applicationName +
+        ' scalaversion: ' + this.scalaVersion);
+    },
 
     end: function() {
         this.log('\r\n');
         this.log('Your project is now created');
         this.log('\r\n');
     }
-
-
-
 });
 
 module.exports = ScalaGenerator;
