b42e6f8ff1a890ae45852655ec57c3ebba2e36b2
Tim Van Wassenhove <github@timvw.be>
2017-04-17 21:32:03 +0200

    wip

---
+++ generators/app/index.js
@@ -0,0 +1,140 @@
+'use strict';
+var Generator = require('yeoman-generator');
+var chalk = require('chalk');
+var fs = require('fs');
+var path = require('path');
+
+/*
+var yosay = require('yosay');
+
+
+
+var mkdirp = require('mkdirp');
+var uuid = require('uuid');
+var request = require('request');
+*/
+
+var greeting =
+  "                          \r\n" +
+  "                _         \r\n" +
+  "  ___  ___ __ _| | __ _   \r\n" +
+  " / __|/ __/ _` | |/ _` |  \r\n" +
+  " \__ \ (_| (_| | | (_| |  \r\n" +
+  " |___/\___\__,_|_|\__,_|  \r\n" +
+  "                          \r\n" ;
+
+class ScalaGenerator extends Generator {
+
+    init() {
+        console.log(greeting);
+        console.log('Welcome to the ' + chalk.red('Scala') + ' generator!');
+    }
+
+    _getSubDirectories(baseDir) {
+        return fs.readdirSync(baseDir)
+            .filter(function (file) {
+                return fs.statSync(path.join(baseDir, file)).isDirectory();
+            });
+    }
+
+    askForTemplate() {
+        if (this.args.length >= 1) {
+            this.templateName = this.args[0];
+        } else {
+            var done = this.async();
+            var baseDir = this.sourceRoot();
+            var choices = this._getSubDirectories(baseDir  + "/../../")
+                .filter(function (x) {
+                    return x != "app"
+                })
+                .map(function (x) {
+                    return {name: x, value: x}
+                });
+
+            var prompts = [{
+                type: 'list',
+                name: 'templateName',
+                message: 'What type of application do you want to create?',
+                choices: choices
+            }];
+
+            this.prompt(prompts, function (props) {
+                this.templateName = props.templateName;
+                done();
+            }.bind(this));
+        }
+    }
+
+    askForName() {
+        if (this.args.length >= 2) {
+            this.applicationName = this.args[1];
+        } else {
+            var done = this.async();
+            var prompts = [{
+                name: 'applicationName',
+                message: 'What\'s the name of your application?',
+                default: this.templateName
+            }];
+
+            this.prompt(prompts, function (props) {
+                this.applicationName = props.applicationName;
+                done();
+            }.bind(this));
+        }
+    }
+
+    askForScalaVersion() {
+        if (this.args.length >= 3) {
+            this.scalaVersion = this.args[2];
+        } else {
+            var done = this.async();
+            var prompts = [{
+                type: 'list',
+                name: 'scalaVersion',
+                message: 'Which version of Scala do you want to use?',
+                choices: [
+                    {name: '2.10.4', value: '2.10.4'},
+                    {name: '2.10.5', value: '2.10.5'},
+                    {name: '2.11.7', value: '2.11.7'},
+                    {name: '2.11.8', value: '2.11.8'}],
+                default: '2.11.8'
+            }];
+
+            this.prompt(prompts, function (props) {
+                this.scalaVersion = props.scalaVersion;
+                done();
+            }.bind(this));
+        }
+    }
+
+    _copy(dirPath, targetDirPath) {
+        var files = fs.readdirSync(dirPath);
+        for (var i in files) {
+            var f = files[i];
+            var fp = path.join(dirPath, f);
+            this.log(f);
+            if (fs.statSync(fp).isDirectory()) {
+                var newTargetPath = path.join(targetDirPath, f);
+                this._copy(fp, newTargetPath);
+            }
+            else {
+                var fn = path.join(targetDirPath.replace('ApplicationName', this.applicationName), f.replace('ApplicationName', this.applicationName));
+                this.template(fp, fn, this.templateData);
+            }
+        }
+    }
+
+    writing() {
+        this.templateData = {applicationName: this.applicationName, scalaVersion: this.scalaVersion};
+        var p = path.join(this.sourceRoot(), this.templateName);
+        this._copy(p, this.applicationName);
+    }
+
+    end() {
+        this.log('\r\n');
+        this.log('Your project is now created');
+        this.log('\r\n');
+    }
+}
+
+module.exports = ScalaGenerator;
---
+++ generators/maven/index.js
@@ -0,0 +1,26 @@
+
+var yeoman = require('yeoman-generator');
+
+module.exports = class extends yeoman {
+
+    /*
+    // The name `constructor` is important here
+    constructor(args, opts) {
+        // Calling the super constructor is important so our generator is correctly set up
+        super(args, opts);
+
+        // Next, add your custom code
+        this.option('babel'); // This method adds support for a `--babel` flag
+    }
+    */
+
+    method1() {
+        this.log('method 1 just ran');
+    }
+
+    method2() {
+        this.log('method 2 just ran');
+    }
+
+
+};
