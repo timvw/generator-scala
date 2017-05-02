d2f8bf7cb40455e6e869c2fe647439cc7cb52f2b
Tim Van Wassenhove <github@timvw.be>
2017-05-02 16:13:01 +0200

    modified scalamavengenerator to extend scala generator

--- generators/maven/index.js
+++ generators/maven/index.js
@@ -1,26 +1,64 @@
+'use strict';
 
-var yeoman = require('yeoman-generator');
+var ScalaGenerator = require('../app/index');
+var _ = require('lodash');
 
-module.exports = class extends yeoman {
+class ScalaMavenGenerator extends ScalaGenerator {
 
-    /*
-    // The name `constructor` is important here
     constructor(args, opts) {
-        // Calling the super constructor is important so our generator is correctly set up
         super(args, opts);
+        console.log("running constructor...");
+        this.option('appname', { type: String })
+        this.option('scalaversion', { type: String });
 
-        // Next, add your custom code
-        this.option('babel'); // This method adds support for a `--babel` flag
-    }
-    */
-
-    method1() {
-        this.log('method 1 just ran');
+        this.sayHi();
     }
 
-    method2() {
-        this.log('method 2 just ran');
+    prompting() {
+      var done = this.async();
+      var prompts = [];
+
+      if(!this.options.appname) {
+        prompts.push({
+                type    : 'input',
+                name    : 'name',
+                message : 'Your project name',
+                default : this.appname // Default to current folder name
+        });
+      }
+
+      if(!this.options.scalaversion) {
+        prompts.push({
+            store: true,
+            type: 'list',
+            name: 'scalaVersion',
+            message: 'Which version of Scala do you want to use?',
+            choices: [
+                {name: '2.10.4', value: '2.10.4'},
+                {name: '2.10.5', value: '2.10.5'},
+                {name: '2.11.7', value: '2.11.7'},
+                {name: '2.11.8', value: '2.11.8'},
+                {name: '2.12.1', value: '2.12.1'}],
+                default: '2.12.1'
+        });
+      }
+
+      if(prompts.length > 0) {
+        this.prompt(prompts).then((answers) => {
+          if(answers.appname) { this.options.appname = answers.appname; }
+          if(answers.scalaversion) { this.options.scalaversion = answers.scalaversion; }
+          done();
+        });
+      } else {
+        done();
+      }
     }
 
+    configuring() {
+      this.log("by now the appname is " + this.options.appname);
+      this.log("by now the scalaversion is " + this.options.scalaversion);
+    }
 
-};
+}
+
+module.exports = ScalaMavenGenerator;
