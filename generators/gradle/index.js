62857e55225cd6ec5ee5b23e2e62e2adfc5a754a
Tim Van Wassenhove <github@timvw.be>
2017-05-04 21:59:23 +0200

    gradle support?

---
+++ generators/gradle/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const Common = require('../../common');
+
+module.exports = class extends Common {
+
+    constructor(args, opts) {
+        super(args, opts);
+        this.argument(this.getApplicationNameParameterName(), { type: String, required: false });
+        this.argument(this.getScalaVersionParameterName(), { type: String, required: false });
+    }
+
+    writing() {
+        this.fs.copyTpl( this.templatePath('../../../common/templates/**/*'), this.destinationRoot(), this.options);
+        this.fs.copyTpl( this.templatePath('**/*'), this.destinationRoot(), this.options);
+    }
+};
